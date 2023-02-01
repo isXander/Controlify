08931e9739e1473df10e82630d94a44a4f213f50
isXander <xandersmith2008@gmail.com>
2023-02-01 23:16:34 +0000

    fix list components not scrolling from controller navigation

---
+++ src/main/java/dev/isxander/controlify/mixins/compat/screen/vanilla/AbstractSelectionListMixin.java
@@ -0,0 +1,16 @@
+package dev.isxander.controlify.mixins.compat.screen.vanilla;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import dev.isxander.controlify.Controlify;
+import dev.isxander.controlify.InputMode;
+import net.minecraft.client.gui.components.AbstractSelectionList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(AbstractSelectionList.class)
+public class AbstractSelectionListMixin {
+    @ModifyExpressionValue(method = "setFocused", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/InputType;isKeyboard()Z"))
+    private boolean shouldEnsureEntryVisible(boolean keyboard) {
+        return keyboard || Controlify.getInstance().getCurrentInputMode() == InputMode.CONTROLLER;
+    }
+}
