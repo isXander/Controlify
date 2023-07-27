edb0cce91b865a72a32a874d1ed28d825c28080d
isXander <xandersmith2008@gmail.com>
2023-07-27 20:25:15 +0100

    ✏️ Format unknown HID identifies as hex for easier searching

--- src/main/java/dev/isxander/controlify/hid/HIDIdentifier.java
+++ src/main/java/dev/isxander/controlify/hid/HIDIdentifier.java
@@ -1,4 +1,14 @@
 package dev.isxander.controlify.hid;
 
+import java.util.HexFormat;
+
 public record HIDIdentifier(int vendorId, int productId) {
+    @Override
+    public String toString() {
+        var hex = HexFormat.of().withPrefix("0x");
+        return "HIDIdentifier[" +
+                "vendorId=" + hex.toHexDigits(vendorId) +
+                ", productId=" + hex.toHexDigits(productId) +
+                ']';
+    }
 }
