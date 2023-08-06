31baf3f8f9601fb33a27e82c989d314bc8dc6572
isXander <xandersmith2008@gmail.com>
2023-08-07 00:14:56 +0100

    🐘 Update some dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
 
     alias(libs.plugins.loom)
-    alias(libs.plugins.loom.quiltflower)
+    alias(libs.plugins.loom.vineflower)
 
     alias(libs.plugins.mod.publish.plugin)
     alias(libs.plugins.machete)
@@ -32,6 +32,7 @@ repositories {
     }
     maven("https://jitpack.io")
     maven("https://maven.flashyreese.me/snapshots")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 val testmod by sourceSets.registering {
