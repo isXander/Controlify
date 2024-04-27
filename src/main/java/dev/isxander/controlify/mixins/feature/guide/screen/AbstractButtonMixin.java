96ae801292192c265efe22d2863a135a30363074
isxander <xander@isxander.dev>
2024-04-27 11:52:26 +0100

    Fix crash when pressing B on title screen and button guides not updating

--- src/main/java/dev/isxander/controlify/mixins/feature/guide/screen/AbstractButtonMixin.java
+++ src/main/java/dev/isxander/controlify/mixins/feature/guide/screen/AbstractButtonMixin.java
@@ -1,65 +1,60 @@
 package dev.isxander.controlify.mixins.feature.guide.screen;
 
 import dev.isxander.controlify.Controlify;
+import dev.isxander.controlify.api.bind.ControllerBinding;
 import dev.isxander.controlify.font.BindingFontHelper;
 import dev.isxander.controlify.gui.ButtonGuideRenderer;
+import it.unimi.dsi.fastutil.objects.Object2ObjectArrayMap;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.AbstractButton;
 import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Mixin(AbstractButton.class)
 public abstract class AbstractButtonMixin extends AbstractWidgetMixin implements ButtonGuideRenderer<AbstractButton>, NarratableEntry /* isActive() in AbstractWidgetMixin shadow breaks dev env remapping. must re-implement interface */ {
     @Unique private RenderData<AbstractButton> renderData = null;
-    @Unique private Component controllerMessage;
+
+    @Unique private final Map<ControllerBinding, Component> controllerMessages = new Object2ObjectArrayMap<>(2);
 
     @Override
-    protected Component modifyMessage(Component actualMessage) {
-        if (shouldRender()) {
-            if (controllerMessage == null) updateControllerMessage();
-            return controllerMessage;
-        }
-        return actualMessage;
-    }
-
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void setControllerMessageInitially(int i, int j, int k, int l, Component component, CallbackInfo ci) {
-        updateControllerMessage();
+    protected Component modifyRenderedMessage(Component actualMessage) {
+        return getControllerMessage();
     }
 
     @Override
     protected void catchMessageSet(Component message, CallbackInfo ci) {
-        updateControllerMessage();
+        controllerMessages.clear();
     }
 
     @Override
-    public void setButtonGuide(RenderData<AbstractButton> renderData) {
+    public void controlify$setButtonGuide(RenderData<AbstractButton> renderData) {
         this.renderData = renderData;
     }
 
     @Unique
-    private void updateControllerMessage() {
-        getBind().ifPresentOrElse(bind -> {
+    private Component getControllerMessage() {
+        if (!shouldRender())
+            return getMessage();
+
+        return getBind().map(bind -> controllerMessages.computeIfAbsent(bind, b -> {
             var component = Component.empty();
             if (!Minecraft.getInstance().font.isBidirectional()) {
-                component.append(BindingFontHelper.binding(bind));
+                component.append(BindingFontHelper.binding(bind.id()));
                 component.append(" ");
             }
-            component.append(getActualMessage());
+            component.append(getMessage());
             if (Minecraft.getInstance().font.isBidirectional()) {
                 component.append(" ");
-                component.append(BindingFontHelper.binding(bind));
+                component.append(BindingFontHelper.binding(bind.id()));
             }
-            controllerMessage = component;
-        }, () -> controllerMessage = null);
+            return component;
+        })).orElse(getMessage());
     }
 
     @Unique
@@ -74,8 +69,8 @@ public abstract class AbstractButtonMixin extends AbstractWidgetMixin implements
     }
 
     @Unique
-    private Optional<ResourceLocation> getBind() {
+    private Optional<ControllerBinding> getBind() {
         if (renderData == null) return Optional.empty();
-        return Controlify.instance().getCurrentController().map(c -> renderData.binding().onController(c).id());
+        return Controlify.instance().getCurrentController().map(c -> renderData.binding().onController(c));
     }
 }
--- src/main/java/dev/isxander/controlify/mixins/feature/guide/screen/AbstractWidgetMixin.java
+++ src/main/java/dev/isxander/controlify/mixins/feature/guide/screen/AbstractWidgetMixin.java
@@ -1,20 +1,16 @@
 package dev.isxander.controlify.mixins.feature.guide.screen;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractWidget.class)
 public abstract class AbstractWidgetMixin {
-    @Shadow
-    private Component message;
 
     @Shadow public abstract int getX();
 
@@ -24,22 +20,15 @@ public abstract class AbstractWidgetMixin {
 
     @Shadow public abstract int getWidth();
 
-    @ModifyArg(method = "renderScrollingString(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/client/gui/Font;II)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/AbstractWidget;renderScrollingString(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Component;IIIII)V"), index = 3)
-    protected int shiftDrawSize(int x) {
-        return x;
-    }
+    @Shadow
+    public abstract Component getMessage();
 
     @Inject(method = "setMessage", at = @At("RETURN"))
     protected void catchMessageSet(Component message, CallbackInfo ci) {
     }
 
-    @ModifyReturnValue(method = "getMessage", at = @At("RETURN"))
-    protected Component modifyMessage(Component actualMessage) {
+    @ModifyExpressionValue(method = "renderScrollingString(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/client/gui/Font;II)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/AbstractWidget;getMessage()Lnet/minecraft/network/chat/Component;"))
+    protected Component modifyRenderedMessage(Component actualMessage) {
         return actualMessage;
-    }
-
-    @Unique
-    protected Component getActualMessage() {
-        return message;
     }
 }
