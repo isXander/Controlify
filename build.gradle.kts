8fe2e211bedcc155793543fd64314bf87c5a0e0d
isxander <xander@isxander.dev>
2024-05-27 00:03:08 +0100

    bump version + changelog

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ val mcSemverVersion = stonecutter.current.version
 val mcDep = property("fmj.mcDep").toString()
 
 group = "dev.isxander"
-val versionWithoutMC = "2.0.0-beta.9"
+val versionWithoutMC = "2.0.0-beta.10"
 version = "$versionWithoutMC+${stonecutter.current.project}"
 
 val isAlpha = "alpha" in version.toString()
