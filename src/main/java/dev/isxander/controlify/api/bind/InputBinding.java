68508525273f0b4c52b40a636348fddebf7dc6ef
isxander <xander@isxander.dev>
2024-05-27 00:02:51 +0100

    JavaDoc for InputBinding

--- src/main/java/dev/isxander/controlify/api/bind/InputBinding.java
+++ src/main/java/dev/isxander/controlify/api/bind/InputBinding.java
@@ -11,6 +11,7 @@ import dev.isxander.controlify.controller.input.ControllerStateView;
 import dev.isxander.controlify.utils.CUtil;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.ApiStatus;
 
 import java.util.Optional;
 import java.util.Set;
@@ -23,44 +24,196 @@ import java.util.function.Consumer;
  * e.g. {@link #justTapped()}. The outputs are then used to perform the action in the game.
  */
 public interface InputBinding {
+    /**
+     * The ID of the binding, set from {@link InputBindingBuilder#id(ResourceLocation)}.
+     * @return the ID
+     */
     ResourceLocation id();
 
+    /**
+     * The name of the binding.
+     * @return the name
+     */
     Component name();
+
+    /**
+     * The description of the binding.
+     * @return the description
+     */
     Component description();
+
+    /**
+     * The category of the binding.
+     * @return the category
+     */
     Component category();
 
+    /**
+     * The icon glyph of the input
+     * @return the icon component
+     */
     Component inputIcon();
 
+    /**
+     * Create a new state access with the given history required.
+     * Please use the minimum amount of history required.
+     *
+     * @param historyRequired the amount of ticks of history required.
+     * @return the state access
+     */
     StateAccess createStateAccess(int historyRequired);
+
+    /**
+     * Create a new state access with the given history required and push event.
+     * Please use the minimum amount of history required.
+     *
+     * @param historyRequired the amount of ticks of history required.
+     * @param pushEvent the event to call when a new state is pushed.
+     * @return the state access
+     */
     StateAccess createStateAccess(int historyRequired, Consumer<StateAccess> pushEvent);
+
+    /**
+     * Returns the state access created with {@link #createStateAccess(int)}.
+     * This renders it unusable forever.
+     * This frees up history if possible.
+     *
+     * @param stateAccess the state access to return
+     */
     void returnStateAccess(StateAccess stateAccess);
 
+    @ApiStatus.Internal
     void pushState(ControllerStateView state);
+
+    /**
+     * Emulates a tap of this binding.
+     * This is used by the radial menu.
+     */
     void fakePress();
 
+    /**
+     * Set the input that this binding is bound to.
+     *
+     * @param input input to bind
+     */
     void setBoundInput(Input input);
+
+    /**
+     * Get the input that this binding is bound to.
+     *
+     * @return currently bound input
+     */
     Input boundInput();
 
+    /**
+     * Get the default input for this binding.
+     * This can change if the user reloads resources and the default changes
+     * (since it's data-driven).
+     *
+     * @return default input
+     */
     Input defaultInput();
 
+    /**
+     * @return true if the binding is equal to {@link EmptyInput}
+     */
     default boolean isUnbound() {
         return EmptyInput.equals(this.boundInput());
     }
 
+    /**
+     * Gets all contexts associated with this binding,
+     * not just the currently applicable ones.
+     * This is set from {@link InputBindingBuilder#allowedContexts(BindContext...)}.
+     *
+     * @return the contexts
+     */
     Set<BindContext> contexts();
 
+    /**
+     * Returns the radial icon's ID.
+     * If the binding does not have a radial icon, this will return an empty optional.
+     * If empty, consider this binding as not being a radial candidate. It can never be added to the radial menu.
+     * @return the radial icon's ID or an empty optional
+     */
     Optional<ResourceLocation> radialIcon();
 
+    /**
+     * Equivalent to calling
+     * <pre><code>
+     *     StateAccess stateAccess = binding.createStateAccess(0);
+     *     float analogueNow = stateAccess.analogue(0);
+     * </code></pre>
+     *
+     * @return the current analogue state (0-1, never negative),
+     * this tick.
+     */
     float analogueNow();
+
+    /**
+     * Equivalent to calling
+     * <pre><code>
+     *     StateAccess stateAccess = binding.createStateAccess(1);
+     *     float analoguePrev = stateAccess.analogue(1);
+     * </code></pre>
+     *
+     * @return the previous analogue state (0-1, never negative), 1 tick ago
+     */
     float analoguePrev();
 
+    /**
+     * Equivalent to calling
+     * <pre><code>
+     *     StateAccess stateAccess = binding.createStateAccess(0);
+     *     boolean digitalNow = stateAccess.digital(0);
+     * </code></pre>
+     *
+     * @return the current digital state (true or false), this tick
+     */
     boolean digitalNow();
+
+    /**
+     * Equivalent to calling
+     * <pre><code>
+     *     StateAccess stateAccess = binding.createStateAccess(1);
+     *     boolean digitalPrev = stateAccess.digital(1);
+     * </code></pre>
+     *
+     * @return the previous digital state (true or false), 1 tick ago
+     */
     boolean digitalPrev();
 
+    /**
+     * Equivalent to calling
+     * <pre><code>
+     *     binding.digitalNow() && !binding.digitalPrev()
+     * </code></pre>
+     *
+     * @return true if the binding is pressed this tick and not pressed the previous tick
+     */
     boolean justPressed();
+
+    /**
+     * Equivalent to calling
+     * <pre><code>
+     *     !binding.digitalNow() && binding.digitalPrev()
+     * </code></pre>
+     *
+     * @return true if the binding is not pressed this tick and pressed the previous tick
+     */
     boolean justReleased();
+
+    /**
+     * @return true if the binding got pressed, and then released
+     */
     boolean justTapped();
 
+    /**
+     * A more advanced output that returns true after the input was released,
+     * but only if the player has not navigated away from the button where the input was initially pressed.
+     *
+     * @return the output
+     */
     GuiPressOutput guiPressed();
 
     ResourceLocation ANALOGUE_NOW = CUtil.rl("analogue_now");
