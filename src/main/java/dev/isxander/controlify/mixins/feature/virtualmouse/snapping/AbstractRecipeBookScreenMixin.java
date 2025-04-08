41238eba3c85435d248ff8e7757a0ae364f8550f
isxander <xander@isxander.dev>
2025-04-08 01:31:40 +0100

    feat: snap point for recipe book toggle button

--- src/main/java/dev/isxander/controlify/mixins/feature/virtualmouse/snapping/AbstractRecipeBookScreenMixin.java
+++ src/main/java/dev/isxander/controlify/mixins/feature/virtualmouse/snapping/AbstractRecipeBookScreenMixin.java
@@ -1,13 +1,18 @@
 package dev.isxander.controlify.mixins.feature.virtualmouse.snapping;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import dev.isxander.controlify.api.vmousesnapping.SnapPoint;
 import dev.isxander.controlify.virtualmouse.SnapUtils;
+import net.minecraft.client.gui.components.ImageButton;
+import net.minecraft.client.gui.navigation.ScreenPosition;
 import net.minecraft.client.gui.screens.inventory.*;
 import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.function.Consumer;
 
@@ -42,5 +47,37 @@ public abstract class AbstractRecipeBookScreenMixin<T extends AbstractContainerM
                 *///?}
                 consumer
         );
+
+        //? if >=1.21.2 {
+        ScreenPosition recipeBookTogglePos = this.getRecipeBookButtonPosition();
+        //?} else {
+        /*ScreenPosition recipeBookTogglePos = new ScreenPosition(recipeBookToggleButton.getX(), recipeBookToggleButton.getY());
+        *///?}
+        consumer.accept(new SnapPoint(
+                recipeBookTogglePos.x(),
+                recipeBookTogglePos.y(),
+                12
+        ));
     }
+
+    //? if >=1.21.2 {
+    @Shadow
+    protected abstract ScreenPosition getRecipeBookButtonPosition();
+    //?} else {
+    /*@Unique private ImageButton recipeBookToggleButton;
+
+    // We need to capture the button and hold a reference to it
+    // to add a snap point to it
+    @ModifyExpressionValue(
+            method = "init",
+            at = @At(
+                    value = "NEW",
+                    target = "Lnet/minecraft/client/gui/components/ImageButton;"
+            )
+    )
+    private ImageButton modifyRecipeBookToggleButton(ImageButton button) {
+        this.recipeBookToggleButton = button;
+        return button;
+    }
+    *///?}
 }
