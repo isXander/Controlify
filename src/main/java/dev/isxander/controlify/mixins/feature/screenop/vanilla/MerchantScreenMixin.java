b34869b381584f4e51392573b9b9dafd60267e26
isXander <xander@isxander.dev>
2025-05-30 22:32:50 +0100

    refactor: remove unused shadow from MerchantScreenMixin

--- src/main/java/dev/isxander/controlify/mixins/feature/screenop/vanilla/MerchantScreenMixin.java
+++ src/main/java/dev/isxander/controlify/mixins/feature/screenop/vanilla/MerchantScreenMixin.java
@@ -6,14 +6,11 @@ import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.gui.screens.inventory.MerchantScreen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(MerchantScreen.class)
 public class MerchantScreenMixin {
-    @Shadow int scrollOff;
-
     @Unique private double accumulatedScroll = 0;
 
     /**
