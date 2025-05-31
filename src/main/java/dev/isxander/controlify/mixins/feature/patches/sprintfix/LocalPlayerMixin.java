7ea7c89fc20ceca6c2b7bfda8cc4c05477b34b2c
isxander <xander@isxander.dev>
2025-05-31 13:36:26 +0100

    feat: only begin sprinting >=0.5 forward impulse, instead of vanilla >0.00001 - allowing players with toggle-sprint to creep around slowly using analogue movement

---
+++ src/main/java/dev/isxander/controlify/mixins/feature/patches/sprintfix/LocalPlayerMixin.java
@@ -0,0 +1,36 @@
+package dev.isxander.controlify.mixins.feature.patches.sprintfix;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import dev.isxander.controlify.Controlify;
+import net.minecraft.client.player.ClientInput;
+import net.minecraft.client.player.LocalPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(LocalPlayer.class)
+public class LocalPlayerMixin {
+    /**
+     * In vanilla, sprinting can start when the forward impulse is above just
+     * <code>0.00001</code>. This makes little sense when controller players have toggle-sprint
+     * enabled, and they're creeping around with minimal input.
+     * Making this <code>>=0.5</code> instead of <code>>0.00001</code> makes a lot more sense.
+     * <p>
+     * There is no need to hide this behind keyboard movement setting, since keyboards will only ever see a
+     * y of <code>0</code> or <code>1</code>, so the point in between where sprinting begins is irrelevant.
+     * However, we do so anyway just for the sake of preventing any modifications to movement code with this toggle on.
+     *
+     * @param instance the receiver
+     * @param original the original call to {@link ClientInput#hasForwardImpulse()}
+     * @return if the client input state satisfies sprinting
+     */
+    @WrapOperation(method = "canStartSprinting", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/ClientInput;hasForwardImpulse()Z"))
+    private boolean requireHalfImpulse(ClientInput instance, Operation<Boolean> original) {
+        if (Controlify.instance().currentInputMode().isController()
+                && !Controlify.instance().config().globalSettings().shouldUseKeyboardMovement()) {
+            return instance.getMoveVector().y >= 0.5f;
+        }
+
+        return original.call(instance);
+    }
+}
