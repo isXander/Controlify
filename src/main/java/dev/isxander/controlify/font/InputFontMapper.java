c8ddbc4eb0e7cfef2475b2cf9e6e1aead76c98cc
isxander <xander@isxander.dev>
2025-08-04 19:40:04 +0100

    fix builds <1.21.4

--- src/main/java/dev/isxander/controlify/font/InputFontMapper.java
+++ src/main/java/dev/isxander/controlify/font/InputFontMapper.java
@@ -118,6 +118,7 @@ public class InputFontMapper implements SimpleControlifyReloadListener<InputFont
                 .collect(Collectors.joining("+"));
         return Component.literal(literal).withStyle(style -> style
                 .withFont(fontMap.namespace().withPrefix("controller/"))
+                //? if >=1.21.4
                 .withShadowColor(0x00000000) // remove shadow
                 .withColor(0xFFFFFFFF)); // override color of font renderer so the glyph always renders properly
     }
