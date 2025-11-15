fe0db417ae8d3f1cfd1ad4299e7d0d3089672700
Ellet <echo.ellet@gmail.com>
2025-11-15 20:50:28 +0300

    feat: screen processor for NeoForge's ModListScreen to enhance GUI operations

---
+++ src/main/java/dev/isxander/controlify/platform/neoforge/mixins/ModListScreenAccessor.java
@@ -0,0 +1,24 @@
+//? if neoforge {
+/*package dev.isxander.controlify.platform.neoforge.mixins;
+
+import net.minecraft.client.gui.components.Button;
+import net.neoforged.neoforge.client.gui.ModListScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(ModListScreen.class)
+public interface ModListScreenAccessor {
+    @Accessor
+    Button getConfigButton();
+
+    @Accessor
+    Button getOpenModsFolderButton();
+
+    @Accessor
+    Button getDoneButton();
+
+    @Invoker
+    void invokeDisplayModConfig();
+}*/
+//?}
---
+++ src/main/java/dev/isxander/controlify/platform/neoforge/mixins/ModListScreenMixin.java
@@ -0,0 +1,22 @@
+//? if neoforge {
+/*package dev.isxander.controlify.platform.neoforge.mixins;
+
+import dev.isxander.controlify.screenop.ScreenProcessor;
+import dev.isxander.controlify.screenop.ScreenProcessorProvider;
+import dev.isxander.controlify.screenop.compat.neoforge.ModListScreenProcessor;
+import net.neoforged.neoforge.client.gui.ModListScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Mixin(ModListScreen.class)
+public class ModListScreenMixin implements ScreenProcessorProvider {
+    @Unique
+    private final ScreenProcessor<?> processor =
+            new ModListScreenProcessor((ModListScreen) (Object) this);
+
+    @Override
+    public ScreenProcessor<?> screenProcessor() {
+        return this.processor;
+    }
+}*/
+//?}
